442592c79b80a3689c5a5b3ae872b0b8456e41f0
maxeem <maxeemkh1989@gmail.com>
2022-08-05 20:39:51 +0300

    last etc

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\CustomAuthController;
 | Web Routes
 |--------------------------------------------------------------------------
 */
-
+Route::get("/" , [CustomAuthController::class, 'dashboard']);
 Route::get('dashboard', [CustomAuthController::class, 'dashboard']);
 Route::get('login', [CustomAuthController::class, 'index'])->name('login');
 Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
